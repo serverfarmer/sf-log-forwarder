9a850d15ecbd9ddc042c1d7865943d03afd25714
Tomasz Klim <github@tomaszklim.pl>
2016-03-06 23:09:12 +0100

    first version

---
+++ README.md
@@ -0,0 +1,3 @@
+sf-log-forwarder extension provides rsyslog daemon configuration as log
+forwarder (all syslog logs are forwarded to the host defined in $SYSLOG
+variable in /etc/farmconfig file).
---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+. /opt/farm/scripts/init
+. /opt/farm/scripts/functions.custom
+. /opt/farm/scripts/functions.install
+
+
+
+base=/opt/sf-log-forwarder/templates/$OSVER
+
+if [ "$SYSLOG" = "true" ]; then
+	echo "install sf-log-receiver extension instead of sf-log-forwarder"
+	exit 0
+fi
+
+if [ ! -f $base/rsyslog.tpl ] || [ ! -f /etc/rsyslog.conf ]; then
+	echo "skipping rsyslog setup, unsupported operating system version"
+	exit 1
+fi
+
+save_original_config /etc/rsyslog.conf
+
+echo "configuring rsyslog as log forwarder"
+cat $base/rsyslog.tpl |sed s/%%syslog%%/$SYSLOG/g >/etc/rsyslog.conf
+
+service rsyslog restart
